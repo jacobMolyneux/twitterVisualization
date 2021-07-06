199d420f82320dfca640a2950c515d5ae9e13144
Jacob Molyneux <79246480+jacobMolyneux@users.noreply.github.com>
2021-07-06 19:16:16 -0400

    first commit with connection to twitter and data cleaner function

---
+++ twitterConnection.py
@@ -0,0 +1,43 @@
+import requests
+import tweepy
+import pandas
+import json
+ApiKey = 'tMyPK26T4fUaBpPqYvvRMLVxR'
+ApiSecretKey = 'e7hxgFvgDEw5I9e4bSLsMrE2ilTQ4CN0LwPjAG2YOWMlz2LY3M'
+access_token = '879601750830972928-x1sNiUtHCjAamveIhXj0GpG7h9ujV8T'
+access_token_secret = '3Wn7SGynXEPNSd4w8xHmrZCehmbRvSQbLXZRxi98imi0K'
+BearerToken = 'AAAAAAAAAAAAAAAAAAAAAGiKMwEAAAAAWWaRZI95ykmtiOmnoJDOp5eQLVs%3DUPfxzeXvED2ONvwqP4KB8zOeNvP1qZo6ltUlmx5aIysqFgigi3'
+
+
+auth = tweepy.OAuthHandler(ApiKey, ApiSecretKey)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+# retrieve data from twitter
+tweetDictionary = {
+    'Date': '',
+    'tweetText': '',
+}
+
+
+def twitterSearch(topic, count):
+    tweets = api.search(topic, lang='en', count=count)
+    jsonTweets = json.loads(tweets)
+    for tweet in tweets:
+        print(jsonTweets)
+
+
+tweetObject = api.search('bitcoin', lang='en', count=100)
+tweets = tweetObject[0]
+json_str = json.dumps(tweets._json)
+# for i in json_str:
+#     tweetDictionary.update({'Date': i.created_at, 'tweetText': i.text})
+print(tweetDictionary)
+print(json_str)
+for i in tweetObject:
+    print(i.text)
+    print(i.created_at)
+    tweetDictionary.update({'Date': i.created_at, 'tweetText': i.text})
+print(tweetDictionary)
+print('the tweet Dictionary Length is: ' + tweetDictionary.length())
